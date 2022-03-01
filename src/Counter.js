aa84e9a7720e808359f3286fd33fbdff9b738a9f
nattinats <n.szajner@gmail.com>
2022-03-01 07:52:53 +0100

    second commit

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,44 @@
 import React, {Component} from 'react';
 import './Counter.css';
+import Display from './Display';
 
 class Counter extends Component {
 
-render() {
-  let randomNumber = Math.floor(Math.random() * (108 - 1 + 1)+1);
-  return (
-    <div className="counter">
-     Counter:
-     <span className="value">
-       {this.props.initValue}
-     </span>
-  </div>
+constructor(props) {
+super(props);
+
+this.state = {
+  counterValue: this.props.initValue
+};
+
+  }
+  
+
+  changeValue = () => {
+    this.setState((prevState) => {
+      return ({
+        counterValue: prevState.counterValue + 1
+      });
+    });
+    //console.log(`changeValue clicked`);
+
+  }
+
+  render() {
+    
+    return ( 
+    <div className = "counter">
+      Counter:
+      <Display/>
+      <button onClick={this.changeValue}>
+        Add 1
+      </button>
+      </div>
 
 
-  );
+    );
+  }
 }
-} 
 
 
 export default Counter;
---
+++ src/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Display(props) {
+  
+  return (
+    <span className = "value"> {108} 
+    </span> 
+
+  );
+}
+
+
+export default Display;
