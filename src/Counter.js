bd5c90665c00a18d9395fa775f20e0493d4855d6
nattinats <n.szajner@gmail.com>
2022-03-18 09:38:55 +0100

    commmunication

--- src/Counter.js
+++ src/Counter.js
@@ -1,63 +1,85 @@
-import React, {Component} from 'react';
+import React, {
+    Component
+} from 'react';
 import './Counter.css';
+
 import Display from './Display';
+import ButtonsPanel from './ButtonsPanel';
 
 class Counter extends Component {
 
-constructor(props) {
-super(props);
+    constructor(props) {
+        super(props);
 
-this.state = {
-  counterValue: this.props.initValue
-};
+        this.state = {
+            counterValue: this.props.initValue
+        };
 
-  }
-  
+        // binding needed when this.changeValue is a ES5 method
+        // this.changeValue = this.changeValue.bind(this);
+    }
 
-  changeValue = () => {
-    this.setState((prevState) => {
-      return ({
-        counterValue: prevState.counterValue + 1
-      });
-    });
-    //console.log(`changeValue clicked`);
+    //changeValue() { // ES5 method (no this context by default)
 
-  }
+    changeValue = (action) => { // ES6 method
 
-  render() {
-    
-    return ( 
-    <div className = "counter">
-      Counter:
-      <Display/>
-      <button onClick={this.changeValue}>
-        Add 1
-      </button>
-      </div>
+        this.setState((prevState, prevProps) => {
 
 
-    );
-  }
+            let currentCounterValue = prevState.counterValue;
+
+
+            if (action === 'add') {
+                currentCounterValue += 1;
+
+            } else if (action === 'reinit') {
+                currentCounterValue = prevProps.initValue;
+
+            } else {
+                currentCounterValue = 0;
+            }
+            return ({
+                counterValue: currentCounterValue
+
+            });
+
+        });
+
+    }
+
+    render() {
+
+        return ( <
+            div className = "counter" >
+            Counter:
+            <
+            Display displayValue = {
+                this.state.counterValue
+            }
+            />  <
+            ButtonsPanel buttonMethod = {
+                this.changeValue
+            }
+            /> <
+            /div>
+        );
+    }
 }
-
 
 export default Counter;
 
+// function Counter(props) {
 
-/* function Counter(props) {
+//     let randomNumber = Math.floor(Math.random() * (108 - 1 + 1) + 1);
 
+//     return (
+//         <div className="counter">
+//             Counter:
+//             <span className="value">
+//                 {props.initValue}
+//             </span>
+//         </div>
+//     );
+// }
 
-let randomNumber = Math.floor(Math.random() * (108 - 1 + 1)+1);
-
-return (
- <div className="counter">
- Counter:
-<span className="value">
- {props.initValue}
- </span>
- </div>
-
-
- );
-}
-  */
+// export default Counter;
--- src/Display.js
+++ src/Display.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './Display.css';
+
+
+
 
 function Display(props) {
   
   return (
-    <span className = "value"> {108} 
+    <span className = {`value ${(props.displayValue <= 5) ? 'blue' : 'red'}`}>
+      {props.displayValue}
     </span> 
 
   );
 }
 
 
-export default Display;
+export default Display; 
